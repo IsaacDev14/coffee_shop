bff71135d017a895a61f84c5390343641b6c7a92
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-18 17:09:38 +0300

    Implement Coffee class orders and customers relationship methods

--- coffee.py
+++ coffee.py
@@ -1,3 +1,5 @@
+from order import Order
+
 class Coffee:
     def __init__(self, name):
         self.name = name #use property setter for validation
@@ -13,3 +15,10 @@ class Coffee:
             if len(value) < 3:
                 raise ValueError("Name must be at least 3 characters")
             self._name = name
+
+
+        def orders(self):
+            return [order for order in Order._all_orders if order.coffee == self]
+        
+        def customers(self):
+            return list[set(order.customer for order in self.orders())]
